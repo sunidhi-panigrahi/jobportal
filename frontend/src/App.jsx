8711018d3f76be863ffed688f3b0093284c08eab
sunidhi-panigrahi <sunidhipanigrahi@gmail.com>
2025-04-20 00:48:48 +0530

    comment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from './components/admin/PostJob'
 import Applicants from './components/admin/Applicants'
 import ProtectedRoute from './components/admin/ProtectedRoute'
+// this is app
 
 
 const appRouter = createBrowserRouter([
